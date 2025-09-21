f4953af068aedb0a4fe72d8998aa4397ed2e170d
Quique Martin <quiquemartinocana@gmail.com>
2025-09-21 20:26:01 +0100

    Add StyleCop configuration and update DestinationsController documentation

--- src/Web.Api/Controllers/DestinationsController.cs
+++ src/Web.Api/Controllers/DestinationsController.cs
@@ -1,11 +1,15 @@
+// Copyright (c) SeeYouThere. All rights reserved.
+
 using Microsoft.AspNetCore.Mvc;
 using Org.OpenAPITools.Controllers;
 using Org.OpenAPITools.Models;
 
 namespace Web.Api.Controllers;
 
-public class DestinationsController: DestinationsApiController
+/// <inheritdoc />
+public class DestinationsController : DestinationsApiController
 {
+    /// <inheritdoc/>
     public override IActionResult FindCheapestDestination(CheapestDestinationRequest cheapestDestinationRequest)
     {
         throw new NotImplementedException();
